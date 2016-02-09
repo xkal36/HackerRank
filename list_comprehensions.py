14c0582cf6557aa7be60d7646c220c006d48886f
Andrew Winterbotham <awinterbotham02@gmail.com>
2016-02-09 15:22:44 +0000

    Update list_comprehensions.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -2,12 +2,12 @@
 You are given three integers X, Y and Z representing 
 the dimensions of a cuboid. You have to print a list 
 of all possible coordinates on a 3D grid where the sum of 
-Xi ++ Yi + Zi is not equal to N. If X=2, 
+Xi + Yi + Zi is not equal to N. If X = 2, 
 the possible values of Xi can be 0, 1 and 2. 
 The same applies to Y and Z.
 
 Input Format:
-Four integers X,Y,Z and N each on four separate lines, respectively.
+Four integers X, Y, Z and N each on four separate lines, respectively.
 
 Output Format:
 Print the list in lexicographic increasing order.
