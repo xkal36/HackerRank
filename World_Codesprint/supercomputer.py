0cc4451b36414a04d076f0270ff51b6a652c958e
Andrew Winterbotham <awinterbotham02@gmail.com>
2016-02-12 12:57:41 +0000

    Update supercomputer.py

--- World_Codesprint/supercomputer.py
+++ World_Codesprint/supercomputer.py
@@ -52,36 +52,8 @@ Sample Output 2
 25
 """
 
-
 import re
 import sys
-
-
-GRID_HEIGHT = 4
-GRID_WIDTH = 6
-
-# Create a rectangular grid using nested list comprehension 
-# Inner comprehension creates a single row
-EXAMPLE_GRID = [[row + col for col in range(GRID_WIDTH)]
-                           for row in range(GRID_HEIGHT)]
-
-def traverse_grid(start_cell, direction, num_steps):
-    """
-    Function that iterates through the cells in a grid
-    in a linear direction
-    
-    Both start_cell is a tuple(row, col) denoting the
-    starting cell
-    
-    direction is a tuple that contains difference between
-    consecutive cells in the traversal
-    """
-    
-    for step in range(num_steps):
-        row = start_cell[0] + step * direction[0]
-        col = start_cell[1] + step * direction[1]
-        print "Processing cell", (row, col), 
-        print "with value", EXAMPLE_GRID[row][col] 
 
 
 def get_input():
